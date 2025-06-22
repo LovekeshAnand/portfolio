23c2f6ebcdb90b6b2692e9aa1c120db7214002ef
Alex1Shelby <ydrgaming71@gmail.com>
2025-06-22 13:04:45 +0530

    test-commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        huihuihuihuihui
+        let's Go
       </main>
   
   );
