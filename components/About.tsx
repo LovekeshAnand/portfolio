3b56a4378454767b61d0af0ec9ec52fc94dd59f8
LovekeshAnand <lovekeshanand6@gmail.com>
2025-06-24 01:17:39 +0530

    image-add

--- components/About.tsx
+++ components/About.tsx
@@ -3,24 +3,29 @@
 import React from 'react';
 import Image from 'next/image';
 import DownloadButton from './Button';
+import Link from 'next/link';
 export default function AboutSection() {
   return (
     <>  
       <div className='h-screen pl-16  items-center justify-center'>
-       <h1 className='font-serif italic text-[#c40505] text-[100px]  mt-30'>About Me</h1>
-        <div className='text-black text-4xl leading-relaxed mt-10 font-serif italic'>
+       <h1 className='font-serif italic text-[#c40505] text-[100px] mt-30'>About Me</h1>
+        <div className='text-black text-4xl 2xl:text-5xl leading-relaxed mt-10 font-serif italic'>
           <div className='font-manrope'>
             Hi!
           </div>
           <span><span className='font-manrope'>I'm </span><strong className='font-playfair underline'>Lovekesh Anand</strong></span><span className='font-manrope'>, a 19-year-old backend developer from New Delhi.<br /></span>
           <span className='font-manrope'>I specialize in building secure and scalable systems using <br /> modern backend technologies.<br /></span>
           <span className='font-manrope'>Currently pursuing a Bachelor of Technology in <span className='font-playfair underline'>Computer Science</span>.<br /></span>
-          <span className='font-manrope'>I enjoy crafting efficient APIs, managing databases, and handling infrastructure that powers real-world applications.</span>
+          <span className='font-manrope'>I enjoy crafting efficient APIs, managing databases, and handling infrastructure that powers real-world applications.</span> <br />
+          <span className='font-manrope'>Download my <span className='font-playfair underline'><Link href={'https://drive.google.com/file/d/160DFkVwyPLMXsK8zXFSKLuvA8fF6eGoO/view?usp=sharing'} target='blank'> Resume.</Link>
+                <Image
+                    className='ml-[300px] mt-[-20px]'
+                    src="/images/hand_cursor.png"
+                    alt="LA19"
+                    height={70}
+                    width={70} 
+                  /></span></span>
         </div>
-        <div className='flex justify-center '>
-          <DownloadButton />
-        </div>
-        
       </div>
 
     </>
--- components/Button.tsx
+++ components/Button.tsx
@@ -4,7 +4,7 @@ const DownloadButton = () => {
   return (
     <div>
       <button className="btn" style={buttonStyles}>
-        Hover Me!
+        RESUME
       </button>
       <style jsx>{`
         .btn {
@@ -22,17 +22,17 @@ const DownloadButton = () => {
         .btn:hover {
           border-radius: 5px;
           transform: translateY(-10px);
-          box-shadow: 0 7px 0 -2px #f85959,
-                      0 15px 0 -4px #39a2db,
-                      0 16px 10px -3px #39a2db;
+          box-shadow: 0 7px 0 -2px #c40505,
+                      0 15px 0 -4px #e85555,
+                      0 16px 10px -3px #f28888;
         }
 
         .btn:active {
           transition: all 0.2s;
           transform: translateY(-5px);
-          box-shadow: 0 2px 0 -2px #f85959,
-                      0 8px 0 -4px #39a2db,
-                      0 12px 10px -3px #39a2db;
+          box-shadow: 0 2px 0 -2px #c40505,
+                      0 8px 0 -4px #e85555,
+                      0 12px 10px -3px #f28888;
         }
       `}</style>
     </div>
