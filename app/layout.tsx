cca7ea7d8ef0769075136264fd1ba7ec1a5bfef7
LovekeshAnand <lovekeshanand6@gmail.com>
2025-07-04 12:23:47 +0530

    analytics-enabled

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
 
 import "./globals.css";
 import SmoothScrollWrapper from "@/components/SmoothScrollWrapper";
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body>
         <SmoothScrollWrapper>
           {children}
+          <Analytics />
         </SmoothScrollWrapper>
       </body>
     </html>
